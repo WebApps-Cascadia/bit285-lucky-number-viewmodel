686b1848fb2776ec08bae66b4d9ae27f267b8f1e
Joanie Dawson <dawson.joan.m@gmail.com>
2018-02-01 15:25:33 -0800

    First commit

--- bit285-lucky-number-viewmodel/Models/Player.cs
+++ bit285-lucky-number-viewmodel/Models/Player.cs
@@ -9,6 +9,17 @@ namespace bit285_lucky_number_viewmodel.Models
     public class Player
     {
         //TODO: Add the properties neededby the Player entity (including Data Annotations)
-       
+        //Modify the Models/Player.cs class to include properties associated 
+        //with Player Entity (Name, Number, Balance). 
+        //make them public, get setter methods
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public double Balance { get; set; }
+        public int PlayerId { get; set; }
+
+      
+
+
+
     }
 }
