bfde592746e966425da05fd847f44af29987e614
juanita <juanitaag1@gmail.com>
2018-02-02 16:32:32 -0800

    did some changes

--- bit285-lucky-number-viewmodel/Models/Player.cs
+++ bit285-lucky-number-viewmodel/Models/Player.cs
@@ -4,11 +4,33 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+
 namespace bit285_lucky_number_viewmodel.Models
 {
     public class Player
     {
         //TODO: Add the properties neededby the Player entity (including Data Annotations)
+        public int PlayerId { get; set; }
+        
+        //Makes the name required and displays the word Name
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        //display words Lucky Number, makes it required and makes the range to be between 1
+        //and 9 and will display a error message if not
+        [Required]
+        [Display(Name = "Lucky Number")]
+        [Range (1,9, ErrorMessage = "Number must be between 1 and 9.")]
+        public int Number { get; set; }
        
+        //Makes Balance a require field and displays the word Balance
+        //makes the balance be a currency field
+       // [Required]
+        [Display(Name = "Balance")]
+        [DataType(DataType.Currency)]
+        public double Balance { get; set; }
+       
+
     }
 }
--- bit285-lucky-number-viewmodel/ViewModels/SpinViewModel.cs
+++ bit285-lucky-number-viewmodel/ViewModels/SpinViewModel.cs
@@ -2,13 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace bit285_lucky_number_viewmodel.ViewModels
 {
     public class SpinViewModel
     {
+      //  public string Message { get; set; }
         //TODO: Add the properties needed by the Spin View
-       
+        [Display(Name = "SPIN")]
+        public int[] Spinner = new int[3];
+
+       // private int _random;
+        public bool isWinner;
+
+        [Required]
+        [Display(Name = "Lucky Number")]
+        //number is the chosen number and if matches 
+        //one of the spin array numbers is a winner
+        public int Number { get; set; }
+
+        //Makes Balance a require field and displays the word Balance
+        //makes the balance be a currency field
+        // [Required]
+        [Display(Name = "Balance")]
+        [DataType(DataType.Currency)]
+        public double Balance { get; set; }
+
+        //displays a message to the user if the balance is equal
+        //to or less than 0 the game is over
+        public string Message
+        {
+            get
+           {
+               return Balance <= 0 ? "GAME OVER" : "";
+          }
+       }
 
         // This method updates values for Spinner and isWinner based on wheter a Lucky Number was rolled
         public void spin()
